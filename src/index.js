3d6ded31ebc408481d62ccace6b632f14bf28396
mariiiiin913 <88306713+mariiiiin913@users.noreply.github.com>
2021-09-24 14:09:40 +0900

    Added js part

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
 let apiKey = "f349e2ee23209dfceb73379ab0acc0cf";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
