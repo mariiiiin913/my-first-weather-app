6e243a6f9854df4e0ea9f229a9dc789298010c97
mariiiiin913 <88306713+mariiiiin913@users.noreply.github.com>
2021-09-24 14:37:52 +0900

    Updated js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "f349e2ee23209dfceb73379ab0acc0cf";
