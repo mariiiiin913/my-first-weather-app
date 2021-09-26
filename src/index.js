708bd201eb2c2a7958ef77a03fae45db357ab56c
mariiiiin913 <88306713+mariiiiin913@users.noreply.github.com>
2021-09-26 20:33:45 +0900

    Added fahrenheit

--- src/index.js
+++ src/index.js
@@ -31,7 +31,10 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
@@ -56,7 +59,21 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("Tokyo");
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+///convert celsius to fahrenheit
+
+function showFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitlink = document.querySelector("#fahrenheit-link");
+fahrenheitlink.addEventListener("click", showFahrenheit);
+
+search("Tokyo");
